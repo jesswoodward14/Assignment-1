600b7998c14a782688a3e0656a7e2173177fe7f5
Jessica Woodward <jesswoodward14@gmail.com>
2019-01-30 16:36:23 -0500

    Ternary help in NameForm

--- lab1/src/NameForm.js
+++ lab1/src/NameForm.js
@@ -1,37 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+
 class NameForm extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {value: ''};
+  constructor(props) {
+    super(props);
+    this.state = {value: '',isAvail:false, isError:false};
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      handleChange(event) {
-        this.setState({value: event.target.value});
-      }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
 
-      handleSubmit(event) {
-        alert('A name was submitted: ' +  this.state.value);
-        event.preventDefault();
-      }
-
-      render() {
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Name:
-              <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
-        );
-      }
+  handleSubmit(event) {
+  if (this.state.value.match(/^[a-zA-Z]+$/)) {
+      event.preventDefault();
+      this.setState({isAvail:true})
+    } else {
+      event.preventDefaut();
+      this.setState({isError:true})
     }
+    event.preventDefault();
+  }
 
-    ReactDOM.render(
-  <NameForm />,
-  document.getElementById('root')
-);
+  render() {
+    return (
+      <div>
+      {(!this.state.isAvail) ?
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <button>Submit</button>
+        </form> :
+        <div><p>Good morning,
+          {this.state.value}
+        </p></div>
+      } </div>
+      :
+      else {(!this.state.isError) ?
+
+      : <div><p class="error">Error</p></div>
+    );
+  }
+}
+
 export default NameForm;
